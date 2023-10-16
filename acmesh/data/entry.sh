d74d0d678bf7b7a6376fdd5bc3c776b196a420d5
Charles Stephen Thompson <6218059+charlestephen@users.noreply.github.com>
2023-10-16 14:49:39 -0400

    Update entry.sh
    Add option to change Acme Server

--- acmesh/data/entry.sh
+++ acmesh/data/entry.sh
@@ -15,6 +15,7 @@ DNS_ENV_OPTIONS=$(jq -r '.dnsEnvVariables |map("export \(.name)=\(.value|tojson)
 KEY_LENGTH=$(bashio::config 'keylength')
 FULLCHAIN_FILE=$(bashio::config 'fullchainfile')
 KEY_FILE=$(bashio::config 'keyfile')
+SERVER=$(bashio::config 'server')
 
 source <(echo ${DNS_ENV_OPTIONS});
 
@@ -30,6 +31,7 @@ function issue {
     acme.sh --issue ${DOMAIN_ARG} \
         --keylength ${KEY_LENGTH} \
         --dns ${DNS_PROTO} \
+        --server ${SERVER} \
         || { ret=$?; [ $ret -eq ${RENEW_SKIP} ] && return 0 || return $ret ;}
 }
 
